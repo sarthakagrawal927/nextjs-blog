2208831c18dbcab3fbba21f562f76d5b93acfcbd
unknown <sarthakagrawal927@gmail.com>
2020-10-13 17:11:43 +0530

    deletted files XD

--- components/work.js
+++
@@ -1,8 +0,0 @@
-export default function Work(props) {
-  return (
-    <>
-      {props.activity}. Its a type of {props.type} work, requiring{" "}
-      {props.participants} participant(s).
-    </>
-  );
-}
