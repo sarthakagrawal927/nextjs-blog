6f4ac736ec2ed1509026222bbdb958cd4e4958c9
unknown <sarthakagrawal927@gmail.com>
2020-11-10 22:07:42 +0530

    testing google tag

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NR5BCPV');`,
+            }}></script>
+        </Head>
         <body>
           <Main />
           <NextScript />
