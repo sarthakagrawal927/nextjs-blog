12b4c8a0462aa437dc580015e940bba6747953c7
unknown <sarthakagrawal927@gmail.com>
2021-01-23 22:49:09 +0530

    added about page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -15,10 +15,6 @@ export default function Sarthak(props) {
       <GSearch />
       <YoutubeSearch />
       <Pomodoro />
-      <img src='https://wakatime.com/share/@sarthakagrawal927/7466eb9d-adc9-43fa-9bba-8f10836e873f.png' />
-      <figure>
-        <embed src='https://wakatime.com/share/@sarthakagrawal927/c25c97c9-20d5-414e-9054-5469e2052f51.svg' />
-      </figure>
     </Layout>
   );
 }
