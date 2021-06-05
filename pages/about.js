4268be979b805afa29adb70bcabddeab88dc1111
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2021-06-06 01:17:23 +0530

    added charts

--- pages/about.js
+++ pages/about.js
@@ -22,9 +22,17 @@ const about = () => {
         </li>
       </ul>
       Coding Activity
-      <img src='https://wakatime.com/share/@sarthakagrawal927/7466eb9d-adc9-43fa-9bba-8f10836e873f.png' />
       <figure>
         <embed src='https://wakatime.com/share/@sarthakagrawal927/c25c97c9-20d5-414e-9054-5469e2052f51.svg' />
+      </figure>
+      <figure>
+        <embed src='https://wakatime.com/share/@sarthakagrawal927/6cfe5ca5-1641-4e68-86b9-7266a6892e2d.svg' />
+      </figure>
+      <figure>
+        <embed src='https://wakatime.com/share/@sarthakagrawal927/73178f2a-624f-4ffd-8dcd-6c31898ac457.svg' />
+      </figure>
+      <figure>
+        <embed src='https://wakatime.com/share/@sarthakagrawal927/07ba4a46-68bc-4e64-8480-8e4b169a0251.svg' />
       </figure>
     </Layout>
   );
